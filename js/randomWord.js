fd883211d3a656cdcb254b35c398a2400d368be8
kienwei96 <kien_wei@hotmail.com>
2021-09-23 22:01:07 +0800

    call randomword through API

---
+++ js/randomWord.js
@@ -0,0 +1,34 @@
+let randomSentence = [];
+let readySentence = ["start to type this sentence", "press spacebar for empty space"];
+
+const split = () => {
+    for(let i=0; i<randomSentence.length; i++) {
+        let lowerCase = randomSentence[i].toLowerCase();
+        let removeSpecialChar = lowerCase.replace(/[^a-zA-Z ]/g, "");
+        readySentence.push(removeSpecialChar);
+    }
+
+}
+
+
+const getData = () => {
+    $.ajax({
+        url: `https://api.chucknorris.io/jokes/random`
+    }).then(successHandler, errHandler)
+}
+
+const successHandler = (data) => {
+    renderSentence(data)
+    split()
+}
+
+const errHandler = (err) => {
+    console.log('Error!', err)
+};
+
+const renderSentence = (data) => {
+    randomSentence.push(data.value)
+}
+
+
+console.log(randomSentence)
